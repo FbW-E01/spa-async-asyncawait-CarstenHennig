2a7e48997cb92db4777f3edda91bf153a105695c
CarstenHennig <carsten.hennig@gmail.com>
2021-06-28 14:34:41 +0200

    Try to understand. Milad did it for me

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,24 +1,76 @@
 import "../styles/main.scss";
-import "babel-polyfill"
+import "babel-polyfill";
 
 const modal = document.getElementById("myModal");
-let promiseOfModal = new Promise(function (resolve) {
-    window.setTimeout(function () {
-        resolve(modal)
-    }, (1000 * 60));
-});
 
-promiseOfModal.then(function(val) {
-    console.log("User has been on the page for 60 seconds");
-    val.style.display = "block";
-})
+let promiseOfModal = () => {
+  return new Promise(function (resolve) {
+    window.setTimeout(function () {
+      resolve(modal);
+    }, 1000 * 1);
+  });
+};
+
+let getModal = async () => {
+  let result = await promiseOfModal();
+  result.style.display = "block";
+};
+getModal();
 
 modal.addEventListener("click", (e) => {
-    switch(e.target.className) {
-        case "close":
-        case "modal":
-        modal.style.display = "none"
-        break;
-    }
-})
+  switch (e.target.className) {
+    case "close":
+    case "modal":
+      modal.style.display = "none";
+      break;
+  }
+});
 
+const button = document.getElementById("continue");
+button.addEventListener("mouseover", () => {
+  animationComplete();
+});
+
+async function animationComplete() {
+  const btnAnimationEnd = await animationStart(button);
+  btnAnimationEnd.classList.add("btn-info");
+  alert("Continue to subscription");
+}
+function animationStart(e) {
+  return new Promise((resolve) => {
+    e.addEventListener("animationend", () => {
+      resolve(e);
+    });
+  });
+}
+
+// const continueButton = document.getElementById("continue");
+// continueButton.addEventListener("mouseover", () => {
+//   onAnimationComplete();
+// });
+// async function onAnimationComplete() {
+//   const buttonAnimationEnd = await onAnimationStart(continueButton);
+//   buttonAnimationEnd.classList.add("btn-info");
+//   alert("Continue to subscription!");
+// }
+// function onAnimationStart(elem) {
+//   return new Promise((resolve) => {
+//     elem.addEventListener("animationend", () => {
+//       resolve(elem);
+//     });
+//   });
+// }
+
+// promiseOfModal.then(function(val) {
+//     console.log("User has been on the page for 60 seconds");
+//     val.style.display = "block";
+// })
+
+// modal.addEventListener("click", (e) => {
+//     switch(e.target.className) {
+//         case "close":
+//         case "modal":
+//         modal.style.display = "none"
+//         break;
+//     }
+// })
